18fb4d8a61200d3864695711470118265542d157
JF Bastien <jfb@chromium.org>
2016-04-21 13:57:25 -0700

    Color background instead of foreground.

--- print.h
+++ print.h
@@ -16,17 +16,18 @@ NO_INLINE void print(S (&scene_i)[X][Y], S (&scene_z)[X][Y]) {
   constexpr wchar_t shades[] = {L'#', L'X', L'*', L'+', L'-', L'.', L' ', L' '};
   constexpr size_t num_shades = sizeof(shades) / sizeof(shades[0]) - 1;
   constexpr size_t shade_sections = std::numeric_limits<S>::max() / num_shades;
-  constexpr const wchar_t *colors[] = {L"\033[30m", L"\033[31m", L"\033[32m",
-                                       L"\033[33m", L"\033[34m", L"\033[35m",
-                                       L"\033[36m", L"\033[37m", L"\033[37m"};
+  constexpr const wchar_t *colors[] = {L"\033[40m", L"\033[41m", L"\033[42m",
+                                       L"\033[43m", L"\033[44m", L"\033[45m",
+                                       L"\033[46m", L"\033[47m", L"\033[47m"};
   constexpr const wchar_t *reset = L"\033[0m";
   constexpr size_t num_colors = sizeof(colors) / sizeof(colors[0]) - 1;
   constexpr size_t color_sections = max_s / num_colors;
   for (size_t i = 0; i < X; ++i) {
     for (size_t j = 0; j < Y; ++j) {
-      fputws(colors[scene_z[i][j] / color_sections], stdout);
-      putwchar(shades[scene_i[i][j] / shade_sections]);
-      fputws(reset, stdout);
+      auto shade = shades[scene_i[i][j] / shade_sections];
+      if (shade != L' ') fputws(colors[scene_z[i][j] / color_sections], stdout);
+      putwchar(shade);
+      if (shade != L' ') fputws(reset, stdout);
     }
     putwchar(L'\n');
   }
